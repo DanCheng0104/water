e76f1771973baed9f3910fc3a7c44e71c54ed700
dcheng0104@gmail.com <dcheng0104@gmail.com>
2018-03-22 16:57:44 -0700

    barchart

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,7 +10,30 @@ import {color} from '../palette.json'
 mapboxgl.accessToken = token;
 class Map extends React.Component {
     mapContainer = React.createRef();
-
+    panelContainer = React.createRef();
+    state = {    
+      chartData : {
+        labels: ['2011','2012','2013','2014','2015','2016'],
+        datasets: []
+      },
+      options:{
+        scales: {
+          xAxes: [{ stacked: true }],
+          yAxes: [{ stacked: true }]
+        },
+        responsive: true//,
+        // tooltips:{
+        //   callbacks: {
+        //     label: function(tooltipItems, data) {
+        //         //return data.datasets[tooltipItems.datasetIndex].label +': ' + tooltipItems.yLabel + ' €';
+        //         console.log(tooltipItems);
+        //     }
+        // }
+        //}
+      }
+      
+    };
+  
     componentDidUpdate() {
         const filter = ["all",['==','YEAR',this.props.year],['!=','USAGE',-9999],['==','USETYPE','all']];    
         const filter_mask = ["all",['==','YEAR',this.props.year],['==','USAGE',-9999],['==','USETYPE','all']];
@@ -74,23 +97,45 @@ class Map extends React.Component {
       //query results
       this.map.on('click',(e)=>{
         this.props.updateBar(true);
+        this.panelContainer.value.panelRef.value.style["grid-row"]="12/19";
         const features = this.map.queryRenderedFeatures(e.point,{layers:['nb_all']});
-        const usetypes = ["commercial","institutional","other","industrial","res","mixed_use"];
+        const usetypes = ["commercial","institutional","other","industrial","res","mixed_use","masked"];
         const years = [2011,2012,2013,2014,2015,2016];
-        let tempData = {"commercial":[],"institutional":[],"other":[],"industrial":[],"res":[],"mixed_use":[]};
+        let tempData = {"commercial":[],"institutional":[],"other":[],"industrial":[],"res":[],"mixed_use":[],"masked":[]};
+        let colors={"commercial":'#7fc97f',"institutional":'#beaed4',"other":'#fdc086',"industrial":'#ffff99',"res":'#386cb0',"mixed_use":'#f0027f','masked':'#000000'};
         
         usetypes.forEach((usetype)=>{
           years.forEach((year)=>{
             features.forEach((feature)=>{
               if (feature.properties.YEAR == year & feature.properties.USETYPE == usetype){
-                const usage = feature.properties.USAGE == -9999? 0:feature.properties.USAGE;
+                let usage;
+                if (feature.properties.USAGE === -9999){
+                  usage = 100000;
+                  usetype = 'masked';
+                }
+                else{
+                  usage = feature.properties.USAGE;
+
+                }
+                
                 tempData[usetype].push(usage);
               }
             })
           })
         })
-        console.log(tempData);
-        //getChartData
+        let datasets = []
+        Object.keys(tempData).forEach(key=>{
+          //let color = (tempData[key]===100000)?'#000000':colors[key];
+          const dataset = {
+            label: key,
+            data:tempData[key],
+            backgroundColor: colors[key]
+          }
+          datasets.push(dataset)
+        })
+        const newData = {...this.state.chartData};
+        newData.datasets = datasets;
+        this.setState({chartData:newData});
       });
     }
 
@@ -112,7 +157,7 @@ class Map extends React.Component {
         <React.Fragment>
           <div ref={this.mapContainer} >
             <Legend year ={this.props.year} updateYear={this.props.updateYear}/> 
-            <PanelPart barDisplay={this.props.barDisplay} updateBar={this.props.updateBar}/>
+            <PanelPart barDisplay={this.props.barDisplay} updateBar={this.props.updateBar} ref={this.panelContainer} chartData={this.state.chartData} options={this.state.options}/>
           </div>                
         </React.Fragment>
       )
--- src/components/PanelPart.js
+++ src/components/PanelPart.js
@@ -18,46 +18,16 @@ class PanelPart extends Component {
       
     }else{
       e.target.className='glyphicon glyphicon-chevron-down';
-      this.panelRef.value.style["grid-row"]="14/19";
+      this.panelRef.value.style["grid-row"]="12/19";
       this.props.updateBar(true);
       
     }
     
   }
 
-
-  render() {
-      const data = {
-        labels: ['2011','2012'],
-        datasets: [
-          {
-            label: 'Low',
-            data: [67.8,33],
-            backgroundColor: '#D6E9C6',
-          },
-          {
-            label: 'Moderate',
-            data: [20.7,43],
-            backgroundColor: '#FAEBCC',
-          },
-          {
-            label: 'High',
-            data: [11.4,34],
-            backgroundColor: '#EBCCD1',
-          }
-        ]
-      };
-      const options = {
-        scales: {
-          xAxes: [{ stacked: true }],
-          yAxes: [{ stacked: true }]
-        },
-        responsive: true
-        //maintainAspectRatio: false
-      };
-      
-      const barChart = this.props.barDisplay ? (<Bar data ={data}  height={234} width={1100} options = {options}/>):null;
-      const arrow = this.props.barDisplay ? (<span className="glyphicon glyphicon-chevron-down" onClick={(e)=>this.ArrowChange(e)}></span>):(<span className="glyphicon glyphicon-chevron-up" onClick={(e)=>this.ArrowChange(e)}></span>);
+  render() {      
+      const barChart = this.props.barDisplay ? (<Bar data ={this.props.chartData}  height={234} width={1100} options = {this.props.options}/>):null;
+      const arrow = this.props.barDisplay ? (<span className="glyphicon glyphicon-chevron-down"  onClick={(e)=>this.ArrowChange(e)}></span>):(<span className="glyphicon glyphicon-chevron-up" onClick={(e)=>this.ArrowChange(e)}></span>);
     return (      
       <div className='bot-bar' ref={this.panelRef}>
           {arrow}
